084aa53c7a1e23b079e70bab078333b957ed4551
Kelly Camacho <ksophia.martinez@gmail.com>
2025-11-20 18:20:46 -0500

    add success message and count removed IPs

--- allowlist_cleanup.py
+++ allowlist_cleanup.py
@@ -7,13 +7,16 @@ with open("allow_list.txt", "r") as file:
     allowed_ip = file.read().split()
 
 # Remove IPs found in the remove list
+removed_count = 0
 for ip in remove_list:
     if ip in allowed_ip:
         allowed_ip.remove(ip)
+        removed_count += 1
 
 # Write updated allow list back to file
 with open("allow_list.txt", "w") as file:
     file.write("\n".join(allowed_ip))
     
 # Output result
+print(f"{removed_count} IP addresses removed from allow list.")
 print("Allow list successfully updated.")
